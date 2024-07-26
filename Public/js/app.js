5628a917ac36cfefc2f5bde287e0ddf97fc0a303
Nassimakhandari <nassomanassima37@gmail.com>
2024-07-26 21:16:14 +0100

    add email

--- Public/js/app.js
+++ Public/js/app.js
@@ -8,32 +8,48 @@ class dataBase {
         this.password = password
     }
 }
-let user = prompt("signup or login or change the password")
+let user = prompt("Choose sign-up or login or change the password")
 
 let fullName = ""
 
-if (user == "signup") {
+if (user == "sign-up") {
+    // ! Name
     let name = prompt("Enter your Full name")
     let space = name.trim()
     let split = space.split(" ")
-    let letter = split.map(element => element[0].toUpperCase() + element.slice(1).toLowerCase()).join(" ")
+    let fullName = split.map((element => element[0].toUpperCase() + element.slice(1).toLowerCase())).join(" ")
 
-    if (/[^a-zA-Z ]/.test(letter)) {
-        alert("sorry thers a special char in your name");
+    let namelenght = ""
+    while (namelenght < 5) {
+        namelenght = prompt("Enter your Full name")
+        namelenght = namelenght.split(" ").join("").length
     }
-    let name2 = prompt("Enter your Full name")
+    function specialcharacters() {
+        let fullName;
+        while (true) {
+            fullName = prompt("Enter your Full name");
+            if (/[^a-zA-Z ]/.test(fullName)) {
+                alert("Sorry, your name contains special characters");
+            } else {
+                return fullName;
+            }
+        }
+    }
+    specialcharacters()
+    // ! Email
+    let email = prompt('Entrez votre email');
+    while (email.includes(" ") || email.length < 10 || !email.includes("@")) {
+        email = prompt('Email invalide. Entrez votre email à nouveau');
+    }
 
-    if (name.length < 5) {
-        prompt("Enter your Full name")
+    for (let cheked of users) {
+        if (email === cheked.email) {
+            email = prompt('email existe déjà. Entrez un email unique');
+        }
     }
-    let email = prompt("Enter your Email")
     let age = prompt("Enter your Age")
     let password = prompt("Enter your password")
-    
-    let space2 = name2.trim()
-    let split2 = space2.split(" ")
-    let letter2 = split2.map(element => element[0].toUpperCase() + element.slice(1).toLowerCase()).join(" ")
-    fullName = letter2
+
     let sign = new dataBase(fullName, email, age, password)
     users.push(sign)
     console.table(users)
